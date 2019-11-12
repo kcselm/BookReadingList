c7a91e03b516b8e1a04d79913958821d56df4352
kcselm <kcselm93@gmail.com>
2019-11-12 17:24:14 -0600

    Adding test for get_input()

--- test_google_book.py
+++ test_google_book.py
@@ -8,6 +8,14 @@ class TestGoogleBook(unittest.TestCase):
         self.reading_list = [['Mark Lutz', 'David Ascher', 'Learning Python', '"O\'reilly Media, Inc."'], 
                 ['Joseph' 'Eddy' 'Fontenrose', 'Python', 'Biblo & Tannen Publishers']] 
 
+    def test_user_input(self):
+        """
+        Testing get_input().  Gets user input and tests the type of it.  
+        """
+        self.assertIsInstance(google_book.get_input('design patterns'), str)
+        self.assertIsInstance(google_book.get_input(1), str)
+        self.assertIsInstance(google_book.get_input((tuple)), str)
+
     # def test_user_input(self):
     #     with mock.patch('builtins.input', return_value="view") 
     #         assert google_book.get_input()
@@ -17,9 +25,9 @@ class TestGoogleBook(unittest.TestCase):
     #         mocked_get.return_value.ok = True
     #         mocked_get.return_value = 'Success'
             
-    #         books = google_book.get_books()
+    #         google_book.get_books()
     #         mocked_get.assert_called_with('https://www.googleapis.com/books/v1/volumes?q=python&maxResults=5')
-    #         self.assertEqual(books, 'Success')
+    #         self.assertEqual(google_book.get_books(), 'Success')
 
 if __name__ == '__main__':
     unittest.main()
